815f0f6f836a07b74534f7fb582559b85df0f72b
Jash Tailor <59695729+jashtailor@users.noreply.github.com>
2022-01-19 13:24:51 +0530

    Update mk1.py

--- mk1.py
+++ mk1.py
@@ -6,6 +6,7 @@ st.write('Heeeeellooooooooooooo')
 input_text = st.text_input('Enter in Hindi')
 st.write('Enter the sentence in Hindi', input_text)
 
-lang = st.selectbox('Select Language:',('Gujarati', 'Punjabi', 'Kannada', 'Malayalam', 'Odia', 'Bengali'))
-#st.write(UnicodeIndicTransliterator.transliterate(input_text,"hi",lang))
+lang = st.selectbox('Select Language:',('None','Gujarati', 'Punjabi', 'Kannada', 'Malayalam', 'Odia', 'Bengali'))
 st.write('You selected:', lang)
+st.write(UnicodeIndicTransliterator.transliterate(input_text,"hi",lang))
+
