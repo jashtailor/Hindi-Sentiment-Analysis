d291d0d5ad22216109e4942915a4c8aec1860f21
Jash Tailor <59695729+jashtailor@users.noreply.github.com>
2022-01-19 12:58:29 +0530

    Update mk1.py

--- mk1.py
+++ mk1.py
@@ -2,6 +2,7 @@ import streamlit as st
 import indicnlp 
 from indicnlp.transliterate.unicode_transliterate import UnicodeIndicTransliterator
 
+st.write('Heeeeellooooooooooooo')
 title = st.text_input('Movie title', 'Life of Brian')
 st.write('The current movie title is', title)
 
