927841b76d0cab4920333d81bb7c8790192bcdd1
Jash Tailor <59695729+jashtailor@users.noreply.github.com>
2022-01-19 13:30:10 +0530

    Update mk1.py

--- mk1.py
+++ mk1.py
@@ -3,9 +3,8 @@ import indicnlp
 from indicnlp.transliterate.unicode_transliterate import UnicodeIndicTransliterator
 
 st.write('Heeeeellooooooooooooo')
-input_text = st.text_input('Enter in a sentence in Hindi')
-
 lang = st.selectbox('Select Language:',('None','Gujarati', 'Punjabi', 'Kannada', 'Malayalam', 'Odia', 'Bengali'))
+input_text = st.text_input('Enter in a sentence in Hindi')
 
 if lang=='Gujarati':
   st.write(UnicodeIndicTransliterator.transliterate(input_text,"hi",'gu'))
