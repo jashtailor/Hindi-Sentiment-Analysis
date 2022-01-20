417d51f5eb245de36c23f6e1995610be373b7d6d
Jash Tailor <59695729+jashtailor@users.noreply.github.com>
2022-01-20 15:15:26 +0530

    Update mk1.py

--- mk1.py
+++ mk1.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import indicnlp 
 from indicnlp.transliterate.unicode_transliterate import UnicodeIndicTransliterator
+from indicnlp.tokenize import indic_tokenize  
 
 st.write('NLP Mini Project')
 lang = st.selectbox('Select Language:',('None','Gujarati', 'Punjabi', 'Kannada', 'Malayalam', 'Odia', 'Bengali'))
 input_text = st.text_input('Enter in a sentence in Hindi')
 
+input__text = indic_tokenize.trivial_tokenize(input_text) 
 if lang=='Gujarati':
   st.write(UnicodeIndicTransliterator.transliterate(input_text,"hi",'gu'))
 elif lang=='Punjabi':
