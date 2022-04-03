fef6ae3bc92c2c33061d1959744e5bf954283f51
Jash Tailor <59695729+jashtailor@users.noreply.github.com>
2022-04-03 10:41:04 +0530

    Update v2.py

--- v2.py
+++ v2.py
@@ -1,3 +1,4 @@
+# IMPORTING THE NECESSARY LIBRARIES
 import streamlit as st
 
 import pickle
@@ -7,8 +8,22 @@ import numpy as np
 
 from sklearn.ensemble import RandomForestClassifier
 
+from zipfile import ZipFile
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 st.header('''
 Parampara, Pratishtha, Anushasan
 ''')
 
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 input = st.text_input('Enter your sentence in the Hindi Language')
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+RFC = pickle.load(open('model.pkl', 'rb'))
+
+file_name = "hindi_words2.zip"
+with ZipFile(file_name, 'r') as zip1:
+  zip1.extractall()
