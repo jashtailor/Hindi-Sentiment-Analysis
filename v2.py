a3347959b91b5d60faf35160acd9016ec4fb3ef2
Jash Tailor <59695729+jashtailor@users.noreply.github.com>
2022-04-03 10:20:09 +0530

    Update v2.py

--- v2.py
+++ v2.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 import pickle
+
+import pandas as pd
+import numpy as np
+
+from sklearn.ensemble import RandomForestClassifier
+
+st.header('''
+Parampara, Pratishtha, Anushasan
+''')
