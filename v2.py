3a6d7a45268d2e7a8b576621e57c3fca4310e574
Jash Tailor <59695729+jashtailor@users.noreply.github.com>
2022-04-03 10:45:24 +0530

    Update v2.py

--- v2.py
+++ v2.py
@@ -10,6 +10,8 @@ from sklearn.ensemble import RandomForestClassifier
 
 from zipfile import ZipFile
 
+import json
+
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 st.header('''
@@ -27,3 +29,27 @@ RFC = pickle.load(open('model.pkl', 'rb'))
 file_name = "hindi_words2.zip"
 with ZipFile(file_name, 'r') as zip1:
   zip1.extractall()
+
+with open('hindi_words2.json', 'r') as fp:
+  data = json.load(fp)
+  
+def preprocess(text):
+  # 1 -> positive
+  # 0 -> negative
+  # print(text)
+  max_len = 48
+  txt_seq = []
+  lst = []
+  tokenized_input = text.split(' ')
+  for each in tokenized_input:
+    txt_seq.append(data[each])
+  if len(txt_seq)<max_len:
+    difference = max_len - len(txt_seq)
+    # print(difference)
+    zeroes = [0]*difference
+    lst = zeroes + txt_seq
+  # print(lst)
+  lst1 = np.asarray(lst, dtype=np.float64)
+  new_lst1 = lst1.reshape(1, -1)
+  a = RFC.predict(new_lst1)
+  return (a)
